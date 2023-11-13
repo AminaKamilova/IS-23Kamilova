de1cc5a96cfaa14a0bf0ca2240a497f3a3aa5ec9
AminaKamilova <amina.kamilova20@gmail.com>
2023-11-13 17:20:47 +0300

    Merge remote-tracking branch 'origin/master'

--- pz_3/2.py
+++ pz_3/2.py
@@ -1,12 +1,14 @@
 # Дан номер месяца - целое число в диапазоне 1-12
 # (1 - январь, 2 - февраль и т. д.).
 # Определить количество дней в этом месяце для невисокосного года.
-month = int(input('Введите номер месяца: '))
-if month <= 0:
+month = input('Введите номер месяца: ')
+while type(month) != int:
+    try:
+        month = int(month)
+    except ValueError:
+        month = input('Введите номер месяца: ')
+while month <= 0 or month > 12:
     print('Номер месяца должен быть от 1 до 12')
-    month = int(input('Введите номер месяца: '))
-elif isinstance(month, (str, bool)):
-    print('Нужен номер')
     month = int(input('Введите номер месяца: '))
 if month == 2:
     days = 28
