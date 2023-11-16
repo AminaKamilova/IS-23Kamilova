79f87e20cb981a4da887360e2151fcf0b398831c
AminaKamilova <amina.kamilova20@gmail.com>
2023-11-16 19:25:06 +0300

    Merge remote-tracking branch 'origin/master'

--- pz_4/2.py
+++ pz_4/2.py
@@ -1,7 +1,23 @@
-n = input('Введите значение N(N>0): ')
-result = 0
+# Дано целое число N (> 1).
+# Найти наибольшее целое число К, при котором выполняется
+# неравенство 3^k < N.
+n = input('Введите значение N(N>1): ')
 while int != type(n):
     try:
         n = int(n)
     except ValueError:
-        n = input('Введите значение N(N>0): ')
+        n = input('Введите значение N(N>1): ')
+while n <= 1:
+    n = input('Введите значение N(N>1): ')
+    while int != type(n):
+        try:
+            n = int(n)
+        except ValueError:
+            n = input('Введите значение N(N>1): ')
+k = 0
+degree = 3**k
+while degree < n:
+    k = k + 1
+    degree = 3**k
+k = k - 1
+print("Наибольшее целое число К = ", k)
