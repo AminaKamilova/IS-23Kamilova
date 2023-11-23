5cb9851a3d16639ffc1e7f41d6acb13ed595a0ad
AminaKamilova <amina.kamilova20@gmail.com>
2023-11-23 19:43:44 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ pz_5/2(5).py
@@ -0,0 +1,41 @@
+# Описать функцию Minmax(X, Y), записывающую в переменную
+# Х минимальное из значений Хи У, а в переменную У максимальное
+# из этих значений (Х и У вещественные параметры, являющиеся
+# одновременно входными и выходными).
+# Используя четыре вызова этой функции, найти минимальное и
+# максимальное из данных чисел А, В, С, D.
+def Minmax(x, y):
+    if x > y:
+        y, x = x, y
+    return x, y
+
+
+a = input('Введите значение A: ')
+while int != type(a):
+    try:
+        a = int(a)
+    except ValueError:
+        a = input('Введите значение A: ')
+b = input('Введите значение B: ')
+while int != type(b):
+    try:
+        b = int(b)
+    except ValueError:
+        b = input('Введите значение B: ')
+c = input('Введите значение C: ')
+while int != type(c):
+    try:
+        c = int(c)
+    except ValueError:
+        c = input('Введите значение C: ')
+d = input('Введите значение D: ')
+while int != type(d):
+    try:
+        d = int(d)
+    except ValueError:
+        d = input('Введите значение D: ')
+a, b = Minmax(a, b)
+c, d = Minmax(c, d)
+b, d = Minmax(a, d)
+a, c = Minmax(b, c)
+print('Максимальное число: ', d, ', минимальное число: ', a)
