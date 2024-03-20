e20a0937f8dfcd8e335644f1e268c1c6783cde97
AminaKamilova <amina.kamilova20@gmail.com>
2024-03-20 11:21:17 +0300

    Merge remote-tracking branch 'origin/master'

--- pz_14/1(14).py
+++ pz_14/1(14).py
@@ -1,10 +1,10 @@
-# В исходном текстовом файле (Dostoevsky.txt) найти все фамилии с инициалами (например, А. Ф. Куманиной и т.п.)
+# В исходном текстовом файле (Dostoevsky.txt) найти все фамилии с инициалами
+# (например, А. Ф. Куманиной и т.п.)
 import re
 with open('Dostoevsky.txt', 'r', encoding="utf-8") as file:
-        text = file.read()
-        # Найдем фамилии с инициалами
+        file_1 = file.read()
         initials_pattern = r'\b[А-Я]\.\s[А-Я]\.\s[А-Я][а-я]+\b'
-        initials = re.findall(initials_pattern, text)
+        initials = re.findall(initials_pattern, file_1)
         print("Фамилии с инициалами в тексте:")
-        for name in initials:
-            print(name)
+for i in initials:
+    print(i)
